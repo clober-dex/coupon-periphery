e72aba2cf4975b7b574e18ba349b71a29767d88e
Sean <detectivekim1@gmail.com>
2023-11-16 17:30:01 +0900

    refactor: move periphery contracts from `coupon-finance`

--- deploy/Greeter.ts
+++ deploy/Greeter.ts
@@ -1,7 +1,9 @@
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
 
-const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployFunction: DeployFunction = async function (
+  hre: HardhatRuntimeEnvironment,
+) {
   const { deployments, getNamedAccounts } = hre
   const { deploy } = deployments
 
