8214ace6e4f5d3b506b2f8e5fc4677333058c840
JChoy <jchoyeclipse@gmail.com>
2024-03-28 16:09:39 +0900

    feat(deploy): add simple sub deploy task and deploy on arb sepolia

--- task/substitute.ts
+++ task/substitute.ts
@@ -1,7 +1,8 @@
 import { task } from 'hardhat/config'
-import { AAVE_V3_POOL, OWNER, SINGLETON_FACTORY, TOKEN_KEYS, TOKENS, TREASURY, verify } from '../utils'
+import { AAVE_V3_POOL, OWNER, SINGLETON_FACTORY, sleep, TOKEN_KEYS, TOKENS, TREASURY, verify } from '../utils'
 import { hardhat } from 'viem/chains'
 import AaveTokenSubstitute from '../artifacts/contracts/AaveTokenSubstitute.sol/AaveTokenSubstitute.json'
+import SimpleSubstitute from '../artifacts/contracts/SimpleSubstitute.sol/SimpleSubstitute.json'
 import { encodeDeployData, Hex, getCreate2Address, padHex, Address } from 'viem'
 
 task('substitute:aave:deploy')
@@ -42,6 +43,49 @@ task('substitute:aave:deploy')
       })
       console.log(`Deployed ${asset} AaveTokenSubstitute(${computedAddress}) at tx`, transactionHash)
     }
+    await verify(computedAddress, constructorArgs)
+  })
+
+task('substitute:simple:deploy')
+  .addParam('asset', 'name of the asset')
+  .setAction(async ({ asset }, hre) => {
+    const singletonFactory = await hre.viem.getContractAt('ISingletonFactory', SINGLETON_FACTORY)
+    const chainId = hre.network.config.chainId ?? hardhat.id
+    const emptySimpleSubstitute = await hre.viem.getContractAt('SimpleSubstitute', '0x')
+    const treasury = TREASURY[chainId]
+    if (!treasury) {
+      throw new Error('missing treasury')
+    }
+    const underlying = await hre.viem.getContractAt('IERC20Metadata', TOKENS[chainId][asset])
+    const [name, symbol] = await Promise.all([underlying.read.name(), underlying.read.symbol()])
+    const constructorArgs: [string, string, Address, Address, Address] = [
+      'Simple Substitute ' + name,
+      'sub' + symbol,
+      TOKENS[chainId][asset],
+      treasury,
+      OWNER[chainId],
+    ]
+    const deployData = encodeDeployData({
+      abi: emptySimpleSubstitute.abi,
+      args: constructorArgs,
+      bytecode: SimpleSubstitute.bytecode as Hex,
+    })
+    const computedAddress = getCreate2Address({
+      from: singletonFactory.address,
+      bytecode: deployData,
+      salt: padHex('0x0', { size: 32 }),
+    })
+    const client = await hre.viem.getPublicClient()
+    const remoteBytecode = await client.getBytecode({ address: computedAddress })
+    if (remoteBytecode && remoteBytecode !== '0x') {
+      console.log(`${asset} Substitute Contract already deployed:`, computedAddress)
+    } else {
+      const transactionHash = await singletonFactory.write.deploy([deployData, padHex('0x0', { size: 32 })], {
+        gas: 30000000n,
+      })
+      console.log(`Deployed ${asset} SimpleSubstitute(${computedAddress}) at tx`, transactionHash)
+    }
+    await sleep(500)
     await verify(computedAddress, constructorArgs)
   })
 
